78fee2714371da7589893e66644d6401d762c151
motooka <kouma.motooka@tplg.co.jp>
2019-03-07 17:06:04 +0900

    add: about info

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,22 @@
 <template>
     <div>
-        about
+        About
+        <ul>
+            <li>
+                <a href="https://github.com/kouma1990" target="_blank">GitHub</a>
+            </li>
+            <li>
+                <a href="https://atcoder.jp/users/kouma1990" target="_blank">AtCoder</a>
+            </li>
+            <li>
+                <a href="https://www.kaggle.com/kouma1990" target="_blank">Kaggle</a>
+            </li>
+            <li>
+                <a href="https://blog.motimotilab.com/" target="_blank">Blog</a>
+            </li>
+            <li>
+                <a href="https://qiita.com/kouma1990" target="_blank">Qiita</a>
+            </li>
+        </ul>
     </div>
 </template>
